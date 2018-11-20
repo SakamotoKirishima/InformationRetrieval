8a4b3a17b3f1bdb9e24e163c88751b66eef5c330
SakamotoKirishima <38245044+SakamotoKirishima@users.noreply.github.com>
2018-11-20 17:45:18 +0530

    Adding the rest of the search engine

--- Scraper/FileManager.py
+++
@@ -1,9 +0,0 @@
-import os
-class FileManager:
-    @staticmethod
-    def get_all_file_names(dire):
-        file_paths= list()
-        for path, subdirs, files in os.walk(dire):
-            for name in files:
-                file_paths.append(os.path.join(path, name))
-        return file_paths
